bcc118387ba041c0df18860512df73413280cc5c
MiracleCat <miracle_1212@naver.com>
2022-12-24 22:49:31 +0900

    Add programmers - 종이 자르기

---
+++ src/mc/javatest/programmers/level0/PaperCut.java
@@ -0,0 +1,45 @@
+/**
+ * MiracleCat Project
+ * Copyright 2016 https://github.com/miracle134
+ */
+package mc.javatest.programmers.level0;
+
+/**
+ * packageName    : mc.javatest.programmers.level0
+ * fileName       : PaperCut
+ * author         : MiracleCat
+ * date           : 2022-12-24(024)
+ * description    : 종이 자르기
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-12-24(024)        MiracleCat       최초 생성
+ */
+public class PaperCut {
+
+    public static void main(String[] args) {
+        PaperCut paperCut = new PaperCut();
+
+        int N = 5;
+        int M = 2;
+
+        System.out.println(paperCut.solution(M, N));
+    }
+
+    /**
+     * 0 < M, N < 100
+     * 종이를 겹쳐서 자를 수 없습니다.
+     *
+     * @param M 가로길이
+     * @param N 세로길이
+     * @return int
+     */
+    public int solution(int M, int N) {
+        int answer = 0;
+
+        answer = M * N - 1;
+
+        return answer;
+    }
+
+}
--- src/mc/javatest/programmers/level0/SumConsecutiveNumbers.java
+++ src/mc/javatest/programmers/level0/SumConsecutiveNumbers.java
@@ -6,14 +6,13 @@ package mc.javatest.programmers.level0;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 
 /**
  * packageName    : mc.javatest.programmers.level0
  * fileName       : SumConsecutiveNumbers
  * author         : MiracleCat
  * date           : 2022-12-24(024)
- * description    :
+ * description    : 연속된 수의 합
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
@@ -21,7 +20,7 @@ import java.util.LinkedList;
  */
 public class SumConsecutiveNumbers {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SumConsecutiveNumbers sumConsecutiveNumbers = new SumConsecutiveNumbers();
 
         int num = 3;
@@ -33,11 +32,11 @@ public class SumConsecutiveNumbers {
     /**
      * 1 ≤ num ≤ 100
      * 0 ≤ total ≤ 1000
-     * num개의 연속된 수를 더하여 total이 될 수 없는 테스트 케이스는 없습니다.
+     * num 개의 연속된 수를 더하여 total 이 될 수 없는 테스트 케이스는 없습니다.
      *
      * @param num   연속된 수 개수
      * @param total 합
-     * @return
+     * @return int 배열
      */
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
@@ -45,22 +44,24 @@ public class SumConsecutiveNumbers {
         int sum = 0;
 
         // 초기 값
-        for(int i = 1001-num; i <= 1000; i++){
+        for (int i = 1001 - num; i <= 1000; i++) {
             dq.add(i);
-            sum+=i;
+            sum += i;
         }
 
-        int j = 1000-num;
-        while(sum != total){{
-            int tmp = dq.pollLast();
-            dq.addFirst(j);
+        int j = 1000 - num;
+        while (sum != total) {
+            {
+                int tmp = dq.pollLast();
+                dq.addFirst(j);
 
-            sum-=tmp;
-            sum+=j;
-            j--;
-        }}
+                sum -= tmp;
+                sum += j;
+                j--;
+            }
+        }
 
-        for(int i = 0; i < answer.length; i++){
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = dq.pollFirst();
         }
 
